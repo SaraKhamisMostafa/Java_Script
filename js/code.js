f4de7efe3ab14d3666977b43e2091c972031e56d
SaraKhamisMostafa <sarakhamismostafa@gmail.com>
2023-02-08 23:49:20 +0200

    add practice section

---
+++ js/code.js
@@ -0,0 +1,16 @@
+// let name ='mystro'
+// console.log(name);
+// let name='ahamed' ,age=20;
+// console.log(name,age);
+
+//old school way using var instade of let
+// var name = 'ahamed';
+// console.log(name)
+
+//cant use let twice by the variable we just use variable again without let keyword
+
+//i could use back ticks to identify string
+let name='sara'
+let phrase =`can embed another ${name}`
+console.log(phrase)
+alert(`hello, ${name}`)
