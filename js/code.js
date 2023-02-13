4540a11026e1f9e7226464805ed589b360f5e0d6
SaraKhamisMostafa <sarakhamismostafa@gmail.com>
2023-02-13 18:58:39 +0200

    study new session

--- js/code.js
+++ js/code.js
@@ -10,7 +10,69 @@
 //cant use let twice by the variable we just use variable again without let keyword
 
 //i could use back ticks to identify string
-let name='sara'
-let phrase =`can embed another ${name}`
-console.log(phrase)
-alert(`hello, ${name}`)
+// let name='sara'
+// let phrase =`can embed another ${name}`
+// console.log(phrase)
+// alert(`hello, ${name}`)
+
+// let age =7
+// if(age==10){
+//     console.log('ok')
+// }
+// else if(age>5){
+//     console.log('you are ok')
+// }
+// else if(age > 40){
+//     console.log('u are >50')
+// }
+// else{
+//     console.log('you are less than 18')
+// }
+
+// let i=10
+// do{
+//     console.log(i)
+//     i--
+// }while(i>0)
+
+// for(let i=0; i<10;i++){
+    
+//     if(i==5){
+//         continue
+//     } 
+//     console.log(i) 
+// }
+
+
+// for(let m=1; m<6;m++){
+//     for(let i =0; i<=12; i++){
+//         console.log(`${m} X ${i} = ${m*i}`)
+//         }
+// }
+
+// let age =20
+// switch(age){
+//     case 10:
+//         console.log('age=10')
+//         break
+//     case 5:
+//         console.log('age=5')
+//         break   
+//     default :
+//     console.log(age)
+// }
+
+function mysum(){
+    let a=10
+    let b=20
+    console.log(a+b)
+}
+mysum()
+
+function myadd(a,b){
+console.log(a+b)
+}
+myadd(5,8)
+myadd('sara','khamis')
+
+
